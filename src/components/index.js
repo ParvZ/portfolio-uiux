f7b9ac9e3f3a0c22dfcfec5a9022e86c34541bc0
Emran Parvez <pemran@rocketmail.com>
2022-12-15 07:46:31 -0500

    structured some component files

---
+++ src/components/index.js
@@ -0,0 +1,29 @@
+import Navbar from "./Navbar";
+import CardDeal from "./CardDeal";
+import Stats from "./Stats";
+import Billing from "./Billing";
+import Business from "./Business";
+import Button from "./Button";
+import Clients from "./Clients";
+import CTA from "./CTA";
+import FeedbackCard from "./FeedbackCard";
+import Footer from "./Footer";
+import GetStarted from "./GetStarted";
+import Hero from "./Hero";
+import Testimonial from "./Testimonial";
+
+export {
+    Navbar,
+    Billing,
+    CardDeal,
+    Business,
+    Stats,
+    Button,
+    Clients,
+    CTA,
+    FeedbackCard,
+    Footer,
+    GetStarted,
+    Hero,
+    Testimonial
+}
